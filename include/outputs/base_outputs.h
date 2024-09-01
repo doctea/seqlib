a19f7add3e38d95fe879deab4aafb3d32b788298
doctea <doctea@gmail.com>
2024-09-01 14:31:12 +0100

    initial commit of sequencer/euclidian stuff extracted from Microlidian

---
+++ include/outputs/base_outputs.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#define MAX_LABEL 40
+
+#ifdef ENABLE_SCREEN
+    class Menu;
+    #include "menu.h"
+#endif
+
+// class to receive triggers from a sequencer and return values to the owner Processor
+class BaseOutput {
+    public:
+    bool enabled = true;
+
+    char label[MAX_LABEL];
+    BaseOutput (const char *label) {
+        strncpy(this->label, label, MAX_LABEL);
+    }
+    
+    // event_value_1 = send a note on
+    // event_value_2 = send a note off
+    // event_value_3 = ??
+    virtual void receive_event(int_fast8_t event_value_1, int_fast8_t event_value_2, int_fast8_t event_value_3) = 0;
+    virtual void reset() = 0;
+    virtual bool matches_label(const char *compare) {
+        return strcmp(compare, this->label)==0;
+    }
+
+    virtual bool should_go_on() = 0;
+    virtual bool should_go_off() = 0;
+
+    virtual void stop() {};
+    virtual void process() {};
+
+    virtual void loop() {};
+
+    void set_enabled(bool state) {
+        this->enabled = state;
+    }
+    bool is_enabled() {
+        return this->enabled;
+    }
+
+    #ifdef ENABLE_SCREEN
+        virtual void make_menu_items(Menu *menu, int index) {}
+        virtual void make_parameter_menu_items(Menu *menu, int index, uint16_t colour = C_WHITE);
+    #endif
+
+    #ifdef ENABLE_CV_INPUT
+        virtual LinkedList<FloatParameter*> *get_parameters() {
+            return nullptr;
+        }
+    #endif
+};
+
---
+++ src/sequencer/Sequencer.cpp
@@ -0,0 +1,31 @@
+#include "sequencer/Sequencer.h"
+#include "sequencer/Patterns.h"
+
+#include "debug.h"
+
+#include "menu_messages.h"
+
+//#include "outputs/base_output.h"
+
+void BaseSequencer::configure_pattern_output(int index, BaseOutput *output) {
+    if (index >= this->number_patterns) {
+        String message = String("Attempted to configure pattern with invalid index ") + String(index);
+        messages_log_add(message);
+        return;
+    }
+    SimplePattern *p = this->get_pattern(index);
+    if (p!=nullptr)
+        p->set_output(output);
+}
+
+#ifdef ENABLE_SCREEN
+    void BaseSequencer::make_menu_items(Menu *menu) {
+
+    }
+#endif
+
+#ifdef ENABLE_CV_INPUT
+    LinkedList<FloatParameter*> *BaseSequencer::getParameters() {
+        return nullptr;
+    }
+#endif
