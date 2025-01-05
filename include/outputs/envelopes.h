38b8e8efbedcc669af6f1ddec0e700c202f677f5
doctea <doctea@gmail.com>
2025-01-05 22:49:53 +0000

    envelopes now register themselves as parameterinputs, too

--- include/outputs/envelopes.h
+++ include/outputs/envelopes.h
@@ -11,18 +11,16 @@
 
 #include "SinTables.h"
 
-#define SUSTAIN_MINIMUM   1   // was 32         // minimum sustain volume to use (below this becomes inaudible, so cut it off)
-#define ENV_MAX_ATTACK    (PPQN*2) //48 // maximum attack stage length in ticks
-#define ENV_MAX_HOLD      (PPQN*2) //48 // maximum hold stage length
-#define ENV_MAX_DECAY     (PPQN*2) //48 // maximum decay stage length
-#define ENV_MAX_RELEASE   (PPQN*4) //96 // maximum release stage length
-
 #ifdef ENABLE_SCREEN
-#include <LinkedList.h>
+    #include <LinkedList.h>
 #endif
 
 #include "envelopes/envelopes.h"
 #include "envelopes/borolope.h"
+
+#ifdef ENABLE_PARAMETERS
+    #include "parameter_inputs/EnvelopeParameterInput.h"
+#endif
 
 class EnvelopeOutput : public MIDIDrumOutput {
     public:
@@ -38,8 +36,20 @@ class EnvelopeOutput : public MIDIDrumOutput {
             // todo: allow to switch to different types of envelope..?
             this->envelope = new DEFAULT_ENVELOPE_CLASS(
                 label, 
-                [=](uint8_t level) -> void { output_wrapper->sendControlChange(this->midi_cc, level, this->channel); } 
+                [=](float level) -> void { 
+                    output_wrapper->sendControlChange(
+                        this->midi_cc, 
+                        (int8_t)((float)level * 127.0), 
+                        this->channel
+                    ); 
+                } 
             );
+
+            #ifdef ENABLE_PARAMETERS
+                parameter_manager->addInput(
+                    new EnvelopeParameterInput(label, "Env", this->envelope)
+                );
+            #endif
         }
 
     virtual void process() override {
